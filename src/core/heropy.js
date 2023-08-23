bb58ba188a9b93919b7ad6fefb4fe3d3aab46667
kimdayeon37 <kdy37912@naver.com>
2023-08-23 17:53:56 +0900

    hash router 관리

---
+++ src/core/heropy.js
@@ -0,0 +1,53 @@
+// Component //
+export class Component {
+    constructor(payload = {}) {
+        const { tagName = 'div', state = {}, props ={} } = payload
+        this.el = document.createElement(tagName)
+        this.state = state
+        this.props = props
+        this.render()
+    }
+    render() {
+
+    }
+}
+
+// Router //
+function routeRender(routes) {
+    if (!location.hash) {
+        history.replaceState(null, '', '/#/')
+    }
+    const routerView = document.querySelector('router-view')
+    // #/about
+    const [hash, queryString = ''] = location.hash.split('?')
+
+    // a=123&b=456
+    // ['a=123', 'b=456']
+    // { a: '123', b: '456' }
+    const query = queryString
+    .split('&')
+    .reduce((acc, cur) => {
+        const [key, value] = cur.split('=')
+        acc[key] = value
+        return acc
+    }, {})
+    history.replaceState(query, '')
+
+   const currentRoute = routes.find(route => new RegExp(`${route.path}/?$`).test(hash))
+   routerView.innerHTML =''
+   routerView.append(new currentRoute.component().el)
+
+   window.scrollTo(0, 0)
+}
+
+
+
+    
+export function createRouter(routes) {
+    return function() {
+        window.addEventListener('popstate', () => {
+            routeRender(routes)
+        })
+        routeRender(routes)
+    }
+}
---
+++ src/routes/Home.js
@@ -0,0 +1,8 @@
+import { Component } from "../core/heropy";
+
+export default class Home extends Component {
+    render() {
+        this.el.innerHTML = /* html */`
+        <h1>HomePage!</h1>`
+    }
+}
